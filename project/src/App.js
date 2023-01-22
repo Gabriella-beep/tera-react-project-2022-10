5cb6c3c8caf313887d1d5ec74f7cd800180d1bcc
Gabriella-beep <gs43179@gmail.com>
2023-01-22 07:54:39 -0300

    Migra HTML dos componentes restantes

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+import UserBlog from "./components/pages/UserBlog.jsx";
 import Home from "./components/pages/Home";
+import Users from "./components/pages/Users.jsx";
+import CreatePost from "./components/pages/CreatePost.jsx";
+
 
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
-import "./styles/main.css"
+import "./styles/main.css";
+
 
 function App() {
-  return <Home />;
+  return (<CreatePost />
+  )
 }
 
 export default App;
